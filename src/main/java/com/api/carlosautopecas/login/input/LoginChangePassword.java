acad61f144c22866d25f2cc7bddbfdeebdf6aab7
rlangbecker <rlangbecker94@gmail.com>
2023-09-30 19:07:44 -0300

    fix pom

--- src/main/java/com/api/carlosautopecas/login/input/LoginChangePassword.java
+++ src/main/java/com/api/carlosautopecas/login/input/LoginChangePassword.java
@@ -3,7 +3,10 @@ package com.api.carlosautopecas.login.input;
 import lombok.Data;
 
 @Data
-public class LoginChangePassword extends LoginInput{
+public class LoginChangePassword {
 
+    private String login;
+
+    private String senha;
     private String novaSenha;
 }
--- src/main/java/com/api/carlosautopecas/login/input/LoginCreateInput.java
+++ src/main/java/com/api/carlosautopecas/login/input/LoginCreateInput.java
@@ -3,8 +3,11 @@ package com.api.carlosautopecas.login.input;
 import lombok.Data;
 
 @Data
-public class LoginCreateInput extends LoginInput {
+public class LoginCreateInput {
 
+    private String login;
+
+    private String senha;
     private String nome;
 
     private String role;
